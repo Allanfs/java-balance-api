6a8a8f277128909a79996435dde6745011eae46c
Allan Silva <allan.silva@db1.com.br>
2023-06-17 13:17:10 -0300

    refact entity anotations

--- src/main/java/com/github/allanfs/balanceapi/database/entity/Transaction.java
+++ src/main/java/com/github/allanfs/balanceapi/database/entity/Transaction.java
@@ -5,12 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
+@Data
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
