d633dba4d5be679cfcfd8c300de9ea3499212444
Allan Silva <allan.silva.dev@gmail.com>
2023-08-05 21:17:41 -0300

    add find by Created At range

--- src/main/java/com/github/allanfs/balanceapi/database/repository/TransactionRepository.java
+++ src/main/java/com/github/allanfs/balanceapi/database/repository/TransactionRepository.java
@@ -1,9 +1,12 @@
 package com.github.allanfs.balanceapi.database.repository;
 
+import java.util.Date;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.github.allanfs.balanceapi.database.entity.TransactionEntity;
 
 public interface TransactionRepository extends CrudRepository<TransactionEntity, Long>{
-    
+
+    Iterable<TransactionEntity> findAllByCreatedAtBetween(Date start, Date end);
 }
