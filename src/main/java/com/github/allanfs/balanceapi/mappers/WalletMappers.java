a90951550390f88100999bcc71929ccfccf31701
Allan Silva <allan.silva.dev@gmail.com>
2023-08-07 20:47:38 -0300

    Fix build fail in mapper due warnings

--- src/main/java/com/github/allanfs/balanceapi/mappers/WalletMappers.java
+++ src/main/java/com/github/allanfs/balanceapi/mappers/WalletMappers.java
@@ -1,7 +1,6 @@
 package com.github.allanfs.balanceapi.mappers;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.github.allanfs.balanceapi.database.entity.WalletEntity;
@@ -15,10 +14,11 @@ public interface WalletMappers {
 
     Wallet walletEntityToWallet(WalletEntity walletEntity);
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "transactions", ignore = true)
+    // @Mapping(target = "id", ignore = true)
+    // @Mapping(target = "transactions", ignore = true)
     Wallet walletHttpToWallet(WalletHttp walletHttp);
 
+    WalletHttp walletToWalletHttp(Wallet value);
     Iterable<WalletHttp> walletsToWalletsHttp(Iterable<Wallet> wallets);
     Iterable<Wallet> walletsEntityToWallets(Iterable<WalletEntity> wallets);
 
